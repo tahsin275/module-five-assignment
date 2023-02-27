ae3f9ac5f08288fa651a9e632cf8801e6d3661d1
Tahsin Shahriar <tahsin.ewucse@gmail.com>
2023-02-27 19:46:48 +0600

    logic updated

--- process-form.php
+++ process-form.php
@@ -26,19 +26,23 @@ class Person
     }
 }
 
+// Create a new instance of the Person class
+$person = new Person();
+
+// Set the name and email properties using the setName() and setEmail() methods
+$person->setName('Tahsin Shahriar');
+$person->setEmail('tahsin@akg.com');
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
 
-    // Create a new instance of the Person class
-    $person = new Person();
-
-    // Set the name and email properties using the setName() and setEmail() methods
+    // Updating the name and email properties using the setName() and setEmail() methods
     $person->setName($name);
     $person->setEmail($email);
-
-    // Display the name and email properties using the getName() and getEmail() methods
-    echo "Name: " . $person->getName() . "<br>";
-    echo "Email: " . $person->getEmail();
 }
+
+// Display the name and email properties using the getName() and getEmail() methods
+echo "Name: " . $person->getName() . "<br>";
+echo "Email: " . $person->getEmail();
